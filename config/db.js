3cfacbe341765dd7e4fd8daea1019b4b8a8189da
Yosef Yosef <yosi.yosef333@gmail.com>
2026-07-05 18:56:04 +0300

    Configure MongoDB connection and add DNS workaround

--- config/db.js
+++ config/db.js
@@ -1,5 +1,9 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import dns from 'dns';
+
+// Force Node.js to use public DNS servers
+dns.setServers(['8.8.8.8', '1.1.1.1']);
 
 // Load environment variables
 dotenv.config();
